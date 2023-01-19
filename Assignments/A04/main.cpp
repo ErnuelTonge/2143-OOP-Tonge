aaef12bfeaf31b15040ef978526621688d0ea36a
ErnuelTonge <123034903+ErnuelTonge@users.noreply.github.com>
2023-01-19 11:00:06 -0600

    Create main.cpp

---
+++ Assignments/A04/main.cpp
@@ -0,0 +1,119 @@
+/*****************************************************************************
+*                    
+*  Author:           Ernuel Tonge
+*  Email:            ernuel0910@gmail.com
+*  Label:            A04
+*  Title:            Linked List Class
+*  Course:           CMPS 2143
+*  Semester:         Spring 2023
+* 
+*  Description:
+*        This program implements a class that allows a linked list to be used just like 
+*        an array. It overloads the "[]" (square brackets) to simulate accessing seperate 
+*        array elements, but really it traverses the list to find the specified node using
+*        an index value. It also overloads the "+" and "-" signs allowing a user to "add"
+*        or "push" items onto the end of the list, as well as "pop" items off the end of our 
+*        array. This class is not meant to replace the STL vector library, its simply a project
+*        to introduce the beginnings of creating complex / abstract data types. 
+*       
+*        This program implements a list data structure that links together nodes of integers. It does more stuff, but we don't need to write that here.
+*
+*
+*        
+*  Usage: 
+*       - $ ./main filename
+*       - This will read in a file containing whatever values to be read into our list/array. 
+*       
+*  Files:            
+*       main.cpp    : driver program 
+*  
+*****************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+
+class CircularArrayQue {
+private:
+    int *Container;
+    int Front;
+    int Rear;
+    int QueSize; // items in the queue
+    int CurrentSize;
+    void init(int size = 0) {
+        Front = Rear = CurrentSize = 0;
+        QueSize = size;
+    }
+
+    bool Full() {
+        return CurrentSize == QueSize;
+    }
+
+public:
+    CircularArrayQue() {
+        Container = new int[10];
+        init(10);
+    }
+    CircularArrayQue(int size) {
+        Container = new int[size];
+        init(size);
+    }
+
+    void Push(int item) {
+        if (!Full()) {
+            Container[Rear] = item;
+            Rear = (Rear + 1) % QueSize;
+            CurrentSize++;
+        } else {
+            cout << "FULL!!!!" << endl;
+        }
+    }
+
+    int Pop() {
+        int temp = Container[Front];
+        Front = (Front + 1) % QueSize;
+        CurrentSize--;
+        return temp;
+    }
+    friend ostream &operator<<(ostream &os, const CircularArrayQue &other);
+};
+
+ostream &operator<<(ostream &os, const CircularArrayQue &other) {
+
+    for (int i = other.Front; i < other.CurrentSize; i = (i + 1) % other.QueSize) {
+        os << other.Container[i] << " ";
+    }
+    os << endl;
+    return os;
+}
+
+/**
+ * Main Driver
+ * 
+ * For this program, the main driver was used to test the CircularArrayQue class
+ * 
+ */
+int main() {
+    CircularArrayQue C1(5);
+
+    // C1.Push(34);
+    // C1.Push(38);
+    // C1.Push(44);
+    // C1.Push(22);
+    // C1.Push(99);
+    // C1.Push(100);
+
+    C1.Push(1);
+    C1.Push(2);
+    C1.Push(3);
+    // C1.Push(6);
+    // C1.Push(7);
+    cout << C1 << endl;
+
+    // C1.Push(1);
+    // C1.Push(2);
+    // C1.Push(3);
+
+    cout << C1 << endl;
+}
